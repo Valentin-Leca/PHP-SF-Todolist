5641059d697aff646f8adc1ae6a2537f22a0fb42
Valentin Lecavelier Des Etangs <valentin.val78@hotmail.fr>
2023-01-27 18:25:19 +0100

    Fix deprecation & User role

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -98,7 +98,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface {
     }
 
     public function getRoles(): array {
-        return array('ROLE_USER');
+        $roles = $this->roles;
+        $roles[] = "ROLE_USER";
+        return array_unique($roles);
     }
 
     public function eraseCredentials() {
