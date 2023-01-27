ceb712fde3bbd3635c2541041a3dc8038080eeb7
Valentin Lecavelier Des Etangs <valentin.val78@hotmail.fr>
2023-01-27 10:44:45 +0100

    Update to 6.2 over !

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,11 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Response;
 
 class SecurityController extends AbstractController {
 
-    #[Route('/login', name:"login", methods: ['GET'])]
-    public function login(Request $request, AuthenticationUtils $authUtils) {
+    #[Route('/login', name:"login", methods: ['GET', 'POST'])]
+    public function login(Request $request, AuthenticationUtils $authUtils): Response {
 
         return $this->render('security/login.html.twig', array(
             'last_username' => $authUtils->getLastUsername(), // last username entered by the user
@@ -24,7 +25,7 @@ class SecurityController extends AbstractController {
         // This code is never executed.
     }
 
-    #[Route('/logout', name:"logout", methods: ['GET', 'POST'])]
+    #[Route('/logout', name:"logout", methods: ['GET'])]
     public function logoutCheck() {
 
         // This code is never executed.
